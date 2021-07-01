7ecfff92624568c68a4103d969805ff9c2e270bc
farrukh90 <farrukhsadykov@gmail.com>
2021-07-01 23:48:18 +0000

    removed laptop key

--- Class2/ec2.tf
+++ Class2/ec2.tf
@@ -2,5 +2,8 @@ resource "aws_instance" "web" {
   ami           = "ami-0277b52859bac6f4b"
   instance_type = "t3.micro"
   count = 3
+  availability_zone = "us-east-2a"
+  associate_public_ip_address = true
+  monitoring = true 
 }
 

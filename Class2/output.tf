7bbec03e74b0bbda57b0733605b702b849096f1b
farrukh90 <farrukhsadykov@gmail.com>
2021-07-02 01:10:32 +0000

    removed IP

--- Class2/output.tf
+++ Class2/output.tf
@@ -11,15 +11,6 @@ output "AZ"  {
   value  = aws_instance.web[0].availability_zone
 }
 
-
-output "public_ip"  {
-  value  =  [  
-    aws_instance.web[0].public_ip,
-    aws_instance.web[1].public_ip,
-    aws_instance.web[2].public_ip,
-  ]
-}
-
 output "SomeOutput" {
     value = <<EOF
 

bb8e365ec48100fbd98c5e5fee2bc5ad403e335d
farrukh90 <farrukhsadykov@gmail.com>
2021-07-02 01:08:35 +0000

    more output added

--- Class2/output.tf
+++ Class2/output.tf
@@ -19,3 +19,21 @@ output "public_ip"  {
     aws_instance.web[2].public_ip,
   ]
 }
+
+output "SomeOutput" {
+    value = <<EOF
+
+        Machine1 has the following IP address   ${aws_instance.web[0].public_ip}
+        Machine1 has the following ID           ${aws_instance.web[0].id}
+        Machine1 is in                          aws_instance.web[0].availability_zone
+
+        Machine2 has the following IP address   ${aws_instance.web[1].public_ip}
+        Machine2 has the following ID           ${aws_instance.web[1].id}
+        Machine2 is in                          aws_instance.web[0].availability_zone
+
+        Machine3 has the following IP address   ${aws_instance.web[2].public_ip}
+        Machine3 has the following ID           ${aws_instance.web[2].id}
+        Machine3 is in                          aws_instance.web[0].availability_zone
+
+    EOF
+}

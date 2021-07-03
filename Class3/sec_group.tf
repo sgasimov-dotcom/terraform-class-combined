2451999c4bd770375b53d6431bf8c10948431851
farrukh90 <farrukhsadykov@gmail.com>
2021-07-03 21:00:23 +0000

    removed ipv6

--- Class3/sec_group.tf
+++ Class3/sec_group.tf
@@ -27,6 +27,5 @@ resource "aws_security_group" "terraform-combined" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 }

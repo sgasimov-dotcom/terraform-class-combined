dddb37bba334ddde204ed3e1ceb9e94f59d5915f
farrukh90 <farrukhsadykov@gmail.com>
2021-07-07 01:15:57 +0000

    added sec group to an instance

--- Class4/ec2.tf
+++ Class4/ec2.tf
@@ -4,4 +4,5 @@ resource "aws_instance" "web" {
   instance_type = var.instance_type
   key_name      = aws_key_pair.automation.key_name
   tags          = var.key_tags
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
 }

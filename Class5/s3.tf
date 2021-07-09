cef8facf307b87dfaddf471fd80ebf7c3f1226e4
farrukh90 <farrukhsadykov@gmail.com>
2021-07-09 01:54:20 +0000

    added encryption

--- Class5/s3.tf
+++ Class5/s3.tf
@@ -1,3 +1,10 @@
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+
+
 resource "aws_s3_bucket" "bucket2" {
   bucket_prefix = "s3-class-with-evolve-cyber-"
   acl    = "private"
@@ -5,8 +12,16 @@ resource "aws_s3_bucket" "bucket2" {
   versioning {
     enabled = true
   }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.mykey.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
 
-  
+
   tags = {
     Name        = "My bucket"
     Environment = "Dev"

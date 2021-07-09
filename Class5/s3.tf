f6b84380d3c0aa29c8c0217550778b59a5c942e3
farrukh90 <farrukhsadykov@gmail.com>
2021-07-09 01:51:44 +0000

    added s3 versioning

--- Class5/s3.tf
+++ Class5/s3.tf
@@ -2,8 +2,14 @@ resource "aws_s3_bucket" "bucket2" {
   bucket_prefix = "s3-class-with-evolve-cyber-"
   acl    = "private"
 
+  versioning {
+    enabled = true
+  }
+
+  
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
   }
 }
+

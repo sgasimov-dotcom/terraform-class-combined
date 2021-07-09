158e959ddae33a15272cc03403f420dda2c560a4
farrukh90 <farrukhsadykov@gmail.com>
2021-07-09 01:47:47 +0000

    added bucket prefix

--- Class5/s3.tf
+++ Class5/s3.tf
@@ -1,5 +1,5 @@
-resource "aws_s3_bucket" "bucket" {
-  bucket = var.bucket_name
+resource "aws_s3_bucket" "bucket2" {
+  bucket_prefix = "s3-class-with-evolve-cyber-"
   acl    = "private"
 
   tags = {

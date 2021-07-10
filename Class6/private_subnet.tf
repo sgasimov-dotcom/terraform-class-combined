3fa826ebcb78e686794b226a0658e7e0bcab1cdd
farrukh90 <farrukhsadykov@gmail.com>
2021-07-10 20:05:07 +0000

    added private association

--- Class6/private_subnet.tf
+++ Class6/private_subnet.tf
@@ -41,3 +41,20 @@ resource "aws_route_table" "private" {
   }
   tags = var.tags
 }
+
+
+
+resource "aws_route_table_association" "private1" {
+  subnet_id      = aws_subnet.private1.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private2" {
+  subnet_id      = aws_subnet.private2.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private3" {
+  subnet_id      = aws_subnet.private3.id
+  route_table_id = aws_route_table.private.id
+}

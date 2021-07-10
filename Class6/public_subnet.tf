838d3b6845568c8183ea92e63ff098fed2d74ae9
farrukh90 <farrukhsadykov@gmail.com>
2021-07-10 19:50:52 +0000

    added public subnet association

--- Class6/public_subnet.tf
+++ Class6/public_subnet.tf
@@ -36,3 +36,18 @@ resource "aws_route_table" "public" {
   }
   tags = var.tags
 }
+
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public3" {
+  subnet_id      = aws_subnet.public3.id
+  route_table_id = aws_route_table.public.id
+}

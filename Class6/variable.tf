f9aeb78c9c8a8e2f8aa67505cc347e7a0955887a
farrukh90 <farrukhsadykov@gmail.com>
2021-07-10 19:26:25 +0000

    added subnets

--- Class6/variable.tf
+++ Class6/variable.tf
@@ -8,6 +8,6 @@ variable "public_cidr3" {}
 variable "private_cidr1" {}
 variable "private_cidr2" {}
 variable "private_cidr3" {}
-variable "tags" {
+variable "farrukh_tags" {
     type = map
 }
--- Class6/vpc.tf
+++ Class6/vpc.tf
@@ -1,5 +1,7 @@
 resource "aws_vpc" "main" {
   cidr_block       = var.cidr_block
   instance_tenancy = "default"
-  tags = var.tags
+  tags = var.farrukh_tags
 }
+
+

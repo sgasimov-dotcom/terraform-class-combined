8a90208e865ef914359ff39a270effb5c62e7cc3
farrukh90 <farrukhsadykov@gmail.com>
2021-07-10 19:36:42 +0000

    added 2 more subnets

--- Class6/subnet.tf
+++ Class6/subnet.tf
@@ -2,5 +2,23 @@ resource "aws_subnet" "public1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_cidr1
   map_public_ip_on_launch = true
+  availability_zone = "${var.region}a"
+  tags = var.tags
+}
+
+resource "aws_subnet" "public2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_cidr1
+  map_public_ip_on_launch = true
+  availability_zone = "${var.region}b"
+  tags = var.tags
+}
+
+
+resource "aws_subnet" "public3" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_cidr1
+  map_public_ip_on_launch = true
+  availability_zone = "${var.region}c"
   tags = var.tags
 }

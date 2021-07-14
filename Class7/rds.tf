ff652f1f2ed5d142cc25dbe84e81b8ddbdd8d2b1
farrukh90 <farrukhsadykov@gmail.com>
2021-07-14 01:45:44 +0000

    updated subnet group

--- Class7/rds.tf
+++ Class7/rds.tf
@@ -24,6 +24,7 @@ resource "aws_db_instance" "default" {
   name                 = "mydb"
   username             = "foo"
   password             = random_password.password.result
-  db_subnet_group_name = "default"
   parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+
 }

9ca584d48ece02862afface2b06a96be6df82068
farrukh90 <farrukhsadykov@gmail.com>
2021-07-15 23:36:41 +0000

    added subnet group

--- Class8/instance/rds_instance.tf
+++ Class8/instance/rds_instance.tf
@@ -15,3 +15,19 @@ resource "aws_ssm_parameter" "wordpress" {
   type  = "SecureString"
   value = random_password.password.result
 }
+
+
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7.16"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = random_password.password.result
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+  db_subnet_group_name        = "default"
+}

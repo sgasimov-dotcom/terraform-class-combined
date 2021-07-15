25415553047ef36862d409d887d937d445ecb155
farrukh90 <farrukhsadykov@gmail.com>
2021-07-15 23:15:30 +0000

    added password for rds

---
+++ Class8/instance/rds_instance.tf
@@ -0,0 +1,9 @@
+resource "random_password" "password" {
+  length           = 30
+  special          = false
+  override_special = "_%@"
+}
+
+output "password" { 
+    value = random_password.password.result
+}

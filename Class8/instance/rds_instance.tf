6855417028f87fd0b34f99ed20ac4c86eec67b45
farrukh90 <farrukhsadykov@gmail.com>
2021-07-15 23:26:24 +0000

    added wordpress password

--- Class8/instance/rds_instance.tf
+++ Class8/instance/rds_instance.tf
@@ -4,7 +4,14 @@ resource "random_password" "password" {
   override_special = "_%@"
 }
 
-output "password" { 
-    value = random_password.password.result
-    sensitive = true
+output "password" {
+  value     = random_password.password.result
+  sensitive = true
+}
+
+
+resource "aws_ssm_parameter" "wordpress" {
+  name  = "wordpress"
+  type  = "SecureString"
+  value = random_password.password.result
 }

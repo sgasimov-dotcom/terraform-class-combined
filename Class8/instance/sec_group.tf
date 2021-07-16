b3db1b844f725ab1edf168eafb40fbc6b4056e06
farrukh90 <farrukhsadykov@gmail.com>
2021-07-16 00:01:52 +0000

    added sec group

---
+++ Class8/instance/sec_group.tf
@@ -0,0 +1,20 @@
+resource "aws_security_group" "wordpress-db" {
+  name        = "wordpress-db"
+  description = "Allow Mysql traffic to wordpress"
+  ingress {
+    description = "Allow mysql"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = [
+        "0.0.0.0/0",
+        "127.0.0.1/32",
+    ]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}

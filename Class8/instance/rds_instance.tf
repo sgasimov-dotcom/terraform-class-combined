1dffdf42823c1d9f788ef799ccdbbcb66dd7fd96
farrukh90 <farrukhsadykov@gmail.com>
2021-07-16 00:04:32 +0000

    added vpc to class8

--- Class8/instance/rds_instance.tf
+++ Class8/instance/rds_instance.tf
@@ -32,4 +32,7 @@ resource "aws_db_instance" "default" {
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
   db_subnet_group_name = aws_db_subnet_group.default.name
+  vpc_security_group_ids = [
+    aws_security_group.wordpress-db.id
+  ]
 }

87a26d3d80e8239cb7262a5185720ff7dabd575d
farrukh90 <farrukhsadykov@gmail.com>
2021-07-23 00:16:37 +0000

    added attachment

--- Class9-10/elb.tf
+++ Class9-10/elb.tf
@@ -21,3 +21,8 @@ resource "aws_elb" "bar" {
   connection_draining_timeout = 400
 }
 
+
+resource "aws_autoscaling_attachment" "asg_attachment_bar" {
+  autoscaling_group_name = aws_autoscaling_group.example.id
+  elb                    = aws_elb.bar.id
+}

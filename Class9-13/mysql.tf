b0ae780b883fcff7c027718169b07983e3e20bd9
farrukh90 <farrukhsadykov@gmail.com>
2021-08-04 01:45:17 +0000

    updated resource group

--- Class9-13/mysql.tf
+++ Class9-13/mysql.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+  name     = "example-resources-mysql"
   location = "West Europe"
 }
 
 resource "azurerm_mysql_server" "example" {
-  name                = "example-mysqlserver"
+  name                = "example-mysqlserver-2"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 

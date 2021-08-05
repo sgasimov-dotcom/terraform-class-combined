cf910e168df3e26f786786cdd85927ea8504e0fa
farrukh90 <farrukhsadykov@gmail.com>
2021-08-05 23:38:19 +0000

    added make file

---
+++ Class9-14/envs/stage.tfvars
@@ -0,0 +1,5 @@
+address_space       = ["192.168.0.0/16"]
+subnet_prefixes     = ["192.168.1.0/24", "192.168.2.0/24", "192.168.3.0/24"]
+subnet_names        = ["subnet1", "subnet2", "subnet3"]
+resource_group_name = "prod"
+location            = "West Europe"
--- Class9-14/provider.tf
+++ Class9-14/provider.tf
@@ -3,26 +3,35 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "my-resources"
-  location = "West Europe"
+  name     = var.resource_group_name
+  location = var.location
 }
 
 module "vnet" {
   source              = "Azure/vnet/azurerm"
   resource_group_name = azurerm_resource_group.example.name
-  address_space       = ["10.0.0.0/16"]
-  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  subnet_names        = ["subnet1", "subnet2", "subnet3"]
-
+  address_space       = var.address_space
+  subnet_prefixes     = var.subnet_prefixes
+  subnet_names        = var.subnet_names
   subnet_service_endpoints = {
     subnet2 = ["Microsoft.Storage", "Microsoft.Sql"],
     subnet3 = ["Microsoft.AzureActiveDirectory"]
   }
-
   tags = {
     environment = "dev"
     costcenter  = "it"
   }
-
   depends_on = [azurerm_resource_group.example]
 }
+
+
+variable "subnet_names" {
+  type = list(any)
+}
+variable "subnet_prefixes" {
+  type = list(any)
+}
+variable "address_space" {
+}
+variable "resource_group_name" {}
+variable "location" {}
